c6c2719620f2c8c6d2e2cd804839cdd8cf498213
Sominhwan <thalsghks@naver.com>
2023-12-30 11:10:31 +0900

    20231230 SMH [메인 공지사항] 배너 추가

--- src/layouts/footer/FooterComponent.vue
+++ src/layouts/footer/FooterComponent.vue
@@ -3,38 +3,46 @@
       class=" text-center d-flex flex-column"
       color="#F5F7FA"
     >
-    <v-row justify="center" no-gutters>
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="black"
-        variant="text"
-        class="mx-2"
-        rounded="xl"
-      >
-        {{ link }}
-      </v-btn>
-    </v-row>
-      <div>
-        <v-btn
-          v-for="icon in icons"
-          :key="icon.icon"
-          class="mx-4"
-          :icon="icon.icon"
-          :style="getButtonStyle(icon)"
-          variant="text"
-        ></v-btn>
-      </div>
-  
-      <div class="pt-0">
-        푸터
-      </div>
-  
-      <v-divider></v-divider>
-  
-      <div>
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </div>
+      <v-container style="width: 50%;">
+        <v-row no-gutters>
+          <v-btn
+            v-for="link in links"
+            :key="link"
+            color="black"
+            variant="text"
+            class="mx-1"
+            rounded="xl"
+          >
+            {{ link }}
+          </v-btn>
+        </v-row>
+        <img class="footer-divider" src="https://cdn.dak.gg/maple/images/footer/icons/divider.svg" alt="divider">
+        <v-row class="mt-2" no-gutters>
+          <v-col cols="10" class="text-left">
+            <span
+              v-for="link in links"
+              :key="link"
+              class="footer-text mr-2 ml-1"
+            >
+              {{ link }}
+            </span>
+            <br/>
+            <span class="footer-sub-text mt-2 ml-1">
+              © All Rights Reserved. Hosted by SMH Inc. Maple.GG is not associated with NEXON Korea.
+            </span>
+          </v-col>
+          <v-col cols="2" class="text-right">
+            <v-icon
+              v-for="icon in icons"
+              :key="icon.icon"
+              class="sns-icon mx-3"
+              :icon="icon.icon"
+              :style="getButtonStyle(icon)"
+              variant="text"
+            ></v-icon>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-footer>
 </template>
 <script>
@@ -42,12 +50,12 @@ import { ref } from 'vue';
 export default {
     setup() {
       const links = ref([
-        'Home',
-        'About Us',
-        'Team',
-        'Services',
-        'Blog',
-        'Contact Us',
+        '주식회사 메이플 지지(OP.GG)',
+        '통신판매업신고',
+        '제2024-부산사하구-000호',
+        '사업자등록번호: 111-11-11111',
+        '051-222-2222',
+        '대표자: 소민환',
       ]);
       const icons = ref([
         {
@@ -84,3 +92,22 @@ export default {
     }
 }
 </script>
+<style scoped>
+  .footer-divider {
+    width: 100%;
+  }
+
+  .footer-text {
+    color: #121214;
+    font-size: 14px;
+  }
+
+  .footer-sub-text {
+    color: #121214;
+    font-size: 12px;
+  }
+
+  .sns-icon {
+    cursor: pointer;
+  }
+</style>
--- src/views/main/MainView.vue
+++ src/views/main/MainView.vue
@@ -1,27 +1,54 @@
 <template>
-    <img src="@/assets/main/main-background.jpg" aspect-ratio="16/9" cover height="500" style="position: relative; width: 100%"/>
+    <img src="@/assets/main/main-background.jpg"  cover height="500" style="position: relative; width: 100%"/>
     <div class="overlay-text">
-      <h2>Op.gg</h2>
+      <div class="main-title">Maple.gg</div>
     </div>
     <v-card
       class="main-search-input"
       width="400"
       style="background-color: rgba(0, 0, 0, 0.0);"
     >
-      <v-text-field
-          variant="solo"
-          bg-color="#323337"
-          placeholder="캐릭터 또는 길드명을 입력하세요."
-          append-inner-icon="mdi-magnify"
-          rounded
-          single-line
-          hide-details
-          maxlength="30"
-          v-model="searchInfoValue"
-          @click:append-inner="searchInfo()"
-          @keyup.enter="searchInfo()"
-      ></v-text-field>
+      <v-form @submit="searchInfo()">
+        <v-text-field
+            variant="solo"
+            bg-color="#323337"
+            placeholder="캐릭터 또는 길드명을 입력하세요."
+            append-inner-icon="mdi-magnify"
+            rounded
+            single-line
+            hide-details
+            maxlength="30"
+            v-model="searchInfoValue"
+            @click:append-inner="searchInfo()"
+            autofocus
+        ></v-text-field>
+        <!-- @keyup.enter="searchInfo()" -->
+      </v-form>
     </v-card>
+    <v-sheet class="notice-sheet" height="75">
+      <v-container class="notice-container" style="width: 50%; height: 100%;">
+        <v-row no-gutters>
+          <v-col cols="12" class="d-flex align-center">
+            <i class="material-icons" style="font-size: 30px; ">campaign</i>
+            <div class="flow-container">
+              <div class="flow-text">
+                  <div class="flow-wrap">공지사항 테스트 입니다22.</div>
+                  <div class="flow-wrap">2023-12-30 연말입니다. </div>
+                  <div class="flow-wrap">Text that flows from right to left. </div>
+                  <div class="flow-wrap">Text that flows from right to left. </div>
+              </div>
+            </div>
+          </v-col>         
+        </v-row>
+      </v-container>
+    </v-sheet>
+    <v-container class="main-container">
+      <v-row>
+        <v-col>
+          
+        </v-col>
+      </v-row>
+    </v-container>
 </template>
 
 <script>
@@ -48,8 +75,9 @@ export default {
   },
   setup(props, context) {
     const searchInfoValue = ref('');
+
     const searchInfo = () => {
-        console.log(searchInfoValue.value)
+      console.log(searchInfoValue.value)
     };
 
     return {
@@ -61,9 +89,13 @@ export default {
 </script>
 
 <style scoped>
+  .main-title {
+    font-family: 'Single Day', cursive;
+    font-size: 80px;
+  }
   .overlay-text {
     position: absolute;
-    top: 200px;
+    top: 150px;
     left: 50%;
     transform: translate(-50%, 0);
     color: white;
@@ -78,5 +110,56 @@ export default {
     top: 300px;
     left: 50%;
     transform: translate(-50%, 0);
+  }
+
+  .notice-sheet {
+    background: rgb(255,255,255);
+    background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(253,187,45,1) 100%);  
+    position: relative; 
+    bottom: 6px;
+  }
+
+  .notice-container {
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+
+  .flow-text {
+    display: flex;
+    flex: 0 0 auto;
+    white-space: nowrap;
+    overflow: hidden;
+    transition: 0.3s;
+    font-size: 2.5rem;
+    font-weight:bold;
+    color: #000;
+  }
+  .flow-text:hover {
+    color: yellow;
+  }
+  .flow-text:hover .flow-wrap {
+    animation-play-state: paused;
+    cursor: pointer;
+  }
+  .flow-wrap {
+    animation: textLoop 10s linear infinite;
+    padding-right: 5.4881vw;
+    font-size: 18px;
+  }
+  
+  @keyframes textLoop {
+    0% {
+      -webkit-transform: translate3d(0, 0, 0);
+      transform: translate3d(0, 0, 0);
+    }
+    100% {
+      -webkit-transform: translate3d(-100%, 0, 0);
+      transform: translate3d(-100%, 0, 0);
+    }
+  }
+
+  .main-container {
+    width: 50%;
   }
 </style>
